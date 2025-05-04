54d64622267a22b785f2f4830cbfaab89ca30fb6
OT <omartemsah99@gmail.com>
2025-05-04 19:35:48 +0300

    Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import ProjectsSections from "../components/ProjectsSections";
@@ -26,6 +27,7 @@ const Home = () => {
       </main>
 
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
